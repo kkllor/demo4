8d95e83ca938e1b9279881fc8cc1b88c735edca5
gaokai <zhanggaokai@hecom.cn>
2019-07-26 09:53:23 +0800

    init

---
+++ src/main/kotlin/demo1.kt
@@ -0,0 +1,14 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+fun main(args: Array<String>) {
+
+    GlobalScope.launch {
+        delay(1000L)
+        println("world")
+    }
+    println("hello,")
+    Thread.sleep(2000L)
+
+}
